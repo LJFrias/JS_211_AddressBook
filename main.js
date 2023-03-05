013d3bf90340ba4840fb806b5f7760049d086902
LJFrias <friaslukej@gmail.com>
2023-03-05 09:55:39 -0800

    finsihed

--- main.js
+++ main.js
@@ -17,21 +17,29 @@ const getContacts = () => {
     .catch (err => console.log(`Error, ${err}`))
     .then (people => {people.map((person, i) => {
         let html = `
-        <h4>${person.name.first}  ${person.name.last}</h4>
+        
+        <h4 class="names">${person.name.first}  ${person.name.last}</h4>
         <img class="portrait" src="${person.picture.large}">
-        <div id="moreInfo">
-            address, email, phone,
-            </div>
-        <button id="show-button" onclick=myFunction()>More Info/ Less Info</button>
+            
+        <div class="moreInfo" id="toggle${i}">
+        Location: ${person.location.city} ${person.location.country} <br>
+        Email: ${person.email}<br>
+        Phone: ${person.phone}
+        </div>
+        <button onclick="showHide(${i})">More Info/LessInfo</button>
+        
         `;
+        
+        
 
     addressList.insertAdjacentHTML("afterbegin", html);
     })
 })
 }
 
-function myFunction() {
-    var x = document.getElementById("show-button");
+function showHide(i) {
+  
+    let x = document.getElementById(`toggle${i}`);
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
